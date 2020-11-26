a938214cde0a1389bf61f25fc89c6b33ede458b5
pamuditha-i <codingissa@gmail.com>
2020-11-26 08:30:05 +0530

    Working version of displaying the list of locations - roughly in 2-3 second intervals - list is generated with the press of the button and list gets cleared with pressing again - list is printed in the console just before the list is cleared

--- lib/main.dart
+++ lib/main.dart
@@ -22,28 +22,89 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  List<LocationResult> locations = [];  //three variables , a list , a stream sub , a boolean var //list of the GPS locations
+  StreamSubscription<LocationResult> streamSubscription; // data streamed by the service
+  bool trackLocation = false;         //true when we track the location using GPS
+
   @override
   initState() {
     super.initState();
     checkGps();
+
+    trackLocation = false;
+    locations = [];
   }
 
-    checkGps() async {
+   getLocations(){
+      if (trackLocation) {
+        setState(() => trackLocation = false);
+        streamSubscription.cancel();      // the part of the function that used to turn off the GPS stream service
+        streamSubscription = null;        // make it zero
+        print(locations);
+        locations = [];
+      }
+      else{                               // if the GPS tracking part is not running this is used to turn it on
+        setState(() => trackLocation = true);
+
+        streamSubscription = Geolocation
+              .locationUpdates(
+            accuracy: LocationAccuracy.best,
+            displacementFilter: 0.0,      // set up the variance when the location updates will come through
+                                          // at zero, it will feed us data regardless of we move our GPS location or not
+            inBackground: false,            // whether we run it in the background or not
+        )
+            .listen((result) {               // this call back fn will get the result & pass it back to us
+          final location = result;        // the final var in the listener
+          setState(() {
+            locations.add(location);      // call the locations and add the new address to it
+          });
+        });
+
+        streamSubscription.onDone(() => setState(() {       // run when the streamsubs is finished
+          trackLocation = false;
+        }));
+      }
+   }
+
+
+
+  checkGps() async {
       final GeolocationResult result = await Geolocation.isLocationOperational();
       if (result.isSuccessful) {
-        print("Success");
+        print("ProcessSucceeded");
       } else {
-        print("Failedd");
+        print("ProcessFailed");
       }
     }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('GeoLocation Example'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("Get Location"),
+            onPressed: getLocations,
+          )
+        ],
       ),
-      body: Container(),
+      body: Center(
+        child: Container(
+          child: ListView(
+            children: locations
+                .map((loc) => ListTile(
+                    title: Text(
+                        "Current Location: ${loc.location.longitude} : ${loc.location.latitude}"),
+                   subtitle: Text(
+                        "Current Altitude: ${loc.location.altitude}"),
+            ))
+              .toList(),
+          )
+        )
+      ),
     );
   }
 }
